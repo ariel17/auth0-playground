55dac957bfe0d67a43c91c867855eb5fbebe7b4a
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-03-22 05:54:14 -0300

    Adds routes from users.

--- api/main.go
+++ api/main.go
@@ -1,13 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/ariel17/auth0-playground/api/users"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	r := gin.Default()
+
+	r := gin.New()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run()
+
+	users.AddRoutes(r)
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
