6fd4210e7e217d17957c7ebbe7c42c284f127d67
Ariel Gerardo Ríos <arielgerardorios@gmail.com>
2020-03-22 05:55:08 -0300

    Adds config package.

---
+++ api/config/values.go
@@ -0,0 +1,18 @@
+package config
+
+import "os"
+
+var (
+	// Auth0Domain is your TENANT + auth0.com.
+	Auth0Domain string
+
+	// Auth0Audience is the Auth0 token IDENTIFIER:
+	// If using an SPA: CLIENT ID.
+	// If using an API: AUDIENCE (Identifier).
+	Auth0Audience string
+)
+
+func init() {
+	Auth0Domain = os.Getenv("AUTH0_DOMAIN")
+	Auth0Audience = os.Getenv("AUTH0_AUDIENCE")
+}
